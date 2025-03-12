acd5f9775c32b362d83d4880d574787c0dc1a04e
Shahriar <mshahriar40@gmail.com>
2025-03-12 23:55:40 +0600

    Version: 0.6 Activity: - Footer text updated.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 mt-50">
             <aside>
-                <p>Copyright © {new Date().getFullYear()} - All right reserved.</p>
+                <p>Copyright © {new Date().getFullYear()} - All rights reserved.</p>
             </aside>
         </footer>
     );
